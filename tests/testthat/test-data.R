b59de384df671ab16642901098992dde3ed29242
Stefan Müller <mullers@tcd.ie>
2022-08-23 15:24:07 +0100

    Add tests for remaining corpora

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -3,6 +3,9 @@ test_that("corpus data objects are current", {
     expect_true(is.corpus(data_corpus_LMRD))
     expect_true(is.corpus(data_corpus_sotu))
     expect_true(is.corpus(data_corpus_ukmanifestos))
+    expect_true(is.corpus(data_corpus_debates))
+    expect_true(is.corpus(data_corpus_TAhotels))
+    expect_true(is.corpus(data_corpus_trumptweets))
 })
 
 test_that("dictionary data objects are current", {
